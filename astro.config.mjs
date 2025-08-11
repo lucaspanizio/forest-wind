2507f51ee135f4324fee4d0c6bec7bda683cd45a
Lucas Panizio <lucas_panizio@hotmail.com>
2025-08-10 22:18:22 -0300

    🧪 ci: add workflow de deploy

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,6 +4,11 @@ import tailwindcss from '@tailwindcss/vite';
 export default defineConfig({
   base: '/forest-wind/',
   publicDir: 'public',
+  output: 'static',
+  outDir: './dist',
+  build: {
+    assets: 'astro',
+  },
   vite: {
     plugins: [tailwindcss()],
   },
